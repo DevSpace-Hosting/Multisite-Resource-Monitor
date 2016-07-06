d69a4a795258b859d3a885dd8f86dae716e08920
Marc Woodyard <MarcWoodyard@users.noreply.github.com>
2016-07-06 13:31:25 -0700

    Update resources-abused.php

--- resources-abused.php
+++ resources-abused.php
@@ -14,15 +14,15 @@ add_action('muplugins_loaded', 'check_subsite_for_resource_abuse');
     {
         //Lets set some variables.
         $space_allowed = get_space_allowed();
-	    $space_used = get_space_used();
+	$space_used = get_space_used();
 	    
-	    if((!is_admin()) && $space_used > $space_allowed)
-	    {
-	        //Looks like we found a rule breaker.
-	        wp_redirect( 'https://YourDomain.com/Error_Page.html' );
-	        exit;
-	    }
-	    //Looks like they're playing by the rules.
+	if((!is_admin()) && $space_used > $space_allowed)
+	{
+		//Looks like we found a rule breaker.
+		wp_redirect( 'https://YourDomain.com/Error_Page.html' );
+		exit;
+	}
+	//Looks like they're playing by the rules.
     }
 
 ?>
