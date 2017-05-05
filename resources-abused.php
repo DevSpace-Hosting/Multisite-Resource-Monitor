08cd3356972bf3c68256df4f9405a5ad98e8adc3
Marc Woodyard <marc.woodyard@fleep.io>
2017-05-05 12:13:53 -0700

    Code Cleanup

--- resources-abused.php
+++ resources-abused.php
@@ -9,20 +9,16 @@ Author URI: https://marcwoodyard.com
 */
 
 add_action('muplugins_loaded', 'check_subsite_for_resource_abuse');
-
-    function check_subsite_for_resource_abuse()
-    {
-        //Lets set some variables.
+    function check_subsite_for_resource_abuse(){
         $space_allowed = get_space_allowed();
-	$space_used = get_space_used();
+		$space_used = get_space_used();
 	    
-	if((!is_admin()) && $space_used > $space_allowed)
-	{
-		//Looks like we found a rule breaker.
-		wp_redirect( 'https://YourDomain.com/Error_Page.html' );
-		exit;
-	}
-	//Looks like they're playing by the rules.
+		if((!is_admin()) && $space_used > $space_allowed) {
+			//Looks like we found a rule breaker.
+			wp_redirect( 'https://YourDomain.com/Error_Page.html' );
+			exit;
+		} else {
+			//Looks like they're playing by the rules.
+		}
     }
-
 ?>
