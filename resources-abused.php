342ca7ee4b31482538d95b1dc9fbd65a5570a9fb
Marc Woodyard <MarcWoodyard@users.noreply.github.com>
2017-12-28 10:14:15 -0800

    New Error Page & Code Updates

--- resources-abused.php
+++ resources-abused.php
@@ -1,24 +1,28 @@
 <?php
 /*
 Plugin Name: Multisite Resource Monitor
-Plugin URI: https://marcwoodyard.com
-Description: Checks to make sure that all subsites aren't abusing network resources.
-Version: 1.0
+Plugin URI: https://github.com/DevSpace-Hosting/Multisite-Resources-Abused
+Description: Checks to make sure that all subsites aren't abusing multisite network resources.
+Version: 12/29/2017
 Author: Marc Woodyard
 Author URI: https://marcwoodyard.com
 */
 
-add_action('muplugins_loaded', 'check_subsite_for_resource_abuse');
-    function check_subsite_for_resource_abuse(){
-        $space_allowed = get_space_allowed();
-		$space_used = get_space_used();
-	    
-		if((!is_admin()) && $space_used > $space_allowed) {
-			//Looks like we found a rule breaker.
-			wp_redirect( 'https://YourDomain.com/Error_Page.html' );
-			exit;
-		} else {
-			//Looks like they're playing by the rules.
-		}
+add_action('wp_loaded', 'check_network_resource_abuse');
+    function check_network_resource_abuse() {	
+
+    	if(($GLOBALS['pagenow'] == 'wp-login.php') == false && is_admin() == false) {
+
+			// Check subsite storage space.
+			if(get_space_used() > get_space_allowed()) {
+				suspend_website_mrm();
+			}
+    	}    	
     }
+
+function suspend_website_mrm() {
+	wp_redirect(plugins_url( 'multisite-resources-abused/website-suspended.html', dirname(__FILE__) ));
+	exit;
+}
+
 ?>
