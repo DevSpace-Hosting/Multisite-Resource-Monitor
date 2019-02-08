60b85b7016dc5ceb66490a0e99a7c76bf28e5c49
Marc Woodyard <MarcWoodyard@users.noreply.github.com>
2019-02-08 14:15:05 -0800

    Code Cleanup

--- plugin.php
+++ plugin.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Multisite Resource Monitor
 Plugin URI: https://github.com/DevSpace-Hosting/Multisite-Resources-Abused
-Description: Checks to make sure that all subsites aren't abusing multisite network resources.
-Version: 12/29/2017
-Author: Marc Woodyard
-Author URI: https://marcwoodyard.com
+Description: Ensures subsites aren't abusing network resources.
+Version: 2-8-2019
+Author: DevSpace Hosting
+Author URI: https://github.com/DevSpace-Hosting
 */
 
 add_action('wp_loaded', 'check_network_resource_abuse');
@@ -14,14 +14,13 @@ add_action('wp_loaded', 'check_network_resource_abuse');
     	if(($GLOBALS['pagenow'] == 'wp-login.php') == false && is_admin() == false) {
 
 			// Check subsite storage space.
-			if(get_space_used() > get_space_allowed()) {
+			if(get_space_used() > get_space_allowed())
 				suspend_website_mrm();
-			}
     	}    	
     }
 
 function suspend_website_mrm() {
-	wp_redirect(plugins_url( 'multisite-resources-abused/website-suspended.html', dirname(__FILE__) ));
+	wp_redirect(plugins_url( 'multisite-resources-abused/html/website-suspended.html', dirname(__FILE__) ));
 	exit;
 }
 
